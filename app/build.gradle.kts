87acdb5db61cb2b59b54bd664cd577160551e786
Tapmediainc <135960987+Tapmediainc@users.noreply.github.com>
2025-07-04 19:07:20 +0700

    update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ apply(from = "static-ips.gradle.kts")
 
 val canonicalVersionCode = 1538
 val canonicalVersionName = "7.40.2"
-val currentHotfixVersion = 0
+val currentHotfixVersion = 2
 val maxHotfixVersions = 100
 
 val keystores: Map<String, Properties?> = mapOf("debug" to loadKeystoreProperties("keystore.debug.properties"))
